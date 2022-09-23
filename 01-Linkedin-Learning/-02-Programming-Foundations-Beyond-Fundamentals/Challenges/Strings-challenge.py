8c9c5f30826e8189cc79f0ff0b829e2062b8c28f
yousefashrafal11 <84685577+yousefashrafal11@users.noreply.github.com>
2022-09-23 13:36:44 +0200

    update

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
@@ -1,1 +1,6 @@
 
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+
+distance_km = float(miles) * 1.609344
+print(f'Distance is {round(distance_km, 3)} km')
